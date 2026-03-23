0fa757891d2bfb1d37ba920a0d39651568a73b72
John Arenales <162364105+JohnPV894@users.noreply.github.com>
2026-03-23 18:39:22 +0100

    Fix syntax error in main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ conectarMongo();
 //PETICION RAIZ 
 app.get('/',async (req,res) => {
     return res.status(200).send({mensaje:"Peticion raiz correcta" })
-    }
+});
 // CREAR un nuevo usuario (POST)
 app.post('/usuarios', async (req, res) => {
     // Los datos del usuario vienen en el cuerpo de la petición
